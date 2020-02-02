32d3a9d07b0a1ac99cc32629872d45b4b5275eb9
Cesar Murillo <namiayumis@gmail.com>
2020-02-02 22:19:54 +0900

    Eventos en React

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -3,10 +3,17 @@ import React from "react";
 const Producto = ({ producto }) => {
   const { id, nombre, precio } = producto;
 
+  //agregar producto al carrito
+  const seleccionarProducto = id => {
+    console.log("comprando ....", id);
+  };
   return (
     <div>
       <h2>{nombre}</h2>
       <p>{precio}</p>
+      <button type="button" onClick={() => seleccionarProducto(id)}>
+        Comprar
+      </button>
     </div>
   );
 };
