4d8e356927c12eb38a505ccbb75e7e9f5b33cdb7
Cesar Murillo <namiayumis@gmail.com>
2020-02-02 17:26:44 +0900

    Creando un Segundo Componente

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Todos los derechos reservados &copy;</p>
+    </footer>
+  );
+};
+
+export default Footer;
